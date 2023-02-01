4c42ce178680fe1668322be4b2070277fd4f493f
JEdwards <jordanscottedwards@gmail.com>
2023-02-01 15:28:11 -0500

    Implementation of access token retrieval in progress

--- Music Advisor/task/src/advisor/Main.java
+++ Music Advisor/task/src/advisor/Main.java
@@ -1,12 +1,24 @@
 package advisor;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
         final Scanner scan = new Scanner(System.in);
         String input = "";
         boolean go = true;
         boolean authorization = false;
+
+        // Examples of mock data
         String[] featuredPlaylists = new String[] {"Mellow Morning", "Wake Up and Smell the Coffee",
                                                     "Monday Motivation", "Songs to Sing in the Shower"};
         String[] newAlbums = new String[] {"Mountains [Sia, Diplo, Labrinth",
@@ -19,17 +31,66 @@ public class Main {
                                                 "Arab Mood Booster",
                                                  "Sunday Stroll"};
 
+        // Spotify Application Fields
         String clientId = "a15be1aafac14bd6b5d0433c486d02a7";
+        String redirectUri = "http://localhost:8080";
 
         while (go) {
             input = scan.nextLine();
             switch (input) {
                 case "auth":
                     authorization = true;
+
+                    // Start HTTP Server and run HTTP Server
+                    HttpServer server = HttpServer.create();
+                    server.bind(new InetSocketAddress(8080), 0);
+                    server.createContext("/",
+                            new HttpHandler() {
+                                public void handle(HttpExchange exchange) throws IOException {
+                                    String code = "";
+                                    String agree = "Got the code. Return back to your program.";
+                                    String cancel = "Authorization code not found. Try again.";
+                                    String query = exchange.getRequestURI().getQuery();
+                                    exchange.sendResponseHeaders(200, code.length());
+                                    exchange.getResponseBody().write(query.getBytes());
+                                    exchange.getResponseBody().close();
+                                }
+                            }
+                    );
+                    server.start();
+
+                    // Create HTTP Client
+                    //HttpClient client = HttpClient.newBuilder().build();
+
+                    // Build Authorization Request Link
                     StringBuilder link = new StringBuilder("https://accounts.spotify.com/authorize?client_id=");
                     link.append(clientId);
-                    link.append("&redirect_uri=http://localhost:8080&response_type=code\n---SUCCESS---\n");
-                    System.out.print(link.toString());
+                    link.append("&redirect_uri=");
+                    link.append(redirectUri);
+                    link.append("&response_type=code");
+
+                    /* Create HTTP Request
+                    HttpRequest request = HttpRequest.newBuilder()
+                            .uri(URI.create(link.toString()))
+                            .GET()
+                            .build();
+
+                    // Send HTTP Request
+                    HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+
+                    StringBuilder link = new StringBuilder("https://accounts.spotify.com/authorize?client_id=");
+                    link.append(clientId);
+                    link.append("&redirect_uri=");
+                    link.append(redirectUri);
+                    link.append("&response_type=code");
+                    link.append("&redirect_uri=http://localhost:8080&response_type=code\n---SUCCESS---\n");*/
+                    System.out.println("use this link to request the access code:");
+                    System.out.println(link.toString());
+                    System.out.println("waiting for code...");
+
+                    //server.stop(10);
+                    // System.out.println("\n---SUCCESS---");
                     break;
                 case "featured":
                     if (!authorization) {
